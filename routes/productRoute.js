3fc1f6069f150beff36b490bfcde0b1118a50b87
erik192 <erik.astrom2@gmail.com>
2021-03-12 10:18:36 +0100

    fixed login to startscreen

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { userStart } = require("../controllers/productController");
+const verifyUser = require("../middleware/verifyUser");
 
 // Startpage
-router.get("/userStart", userStart);
+router.get("/userStart", verifyUser, userStart);
 
 module.exports = router;
