fb3914dff61fc5bab2cd00cec0c0546ae5c2ba68
FredrikAndre <fredrik.he.andre@gmail.com>
2021-03-15 14:17:39 +0100

    Added Pagination

--- middleware/productMiddleware.js
+++ middleware/productMiddleware.js
@@ -4,11 +4,24 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const productMiddleware = async (req,res,next)=>  {
-
+    
+    const page = +req.query.page || 1;
     const token = req.cookies.jwtToken;
-    const products = await Product.find();
-    if(!token) { 
-        return res.render("productPage.ejs", {products} );
+    const totalProducts = await Product.countDocuments();
+    const limitPerPage = 3;
+    const totalPages = Math.ceil(totalProducts / limitPerPage);
+    const productsToShow = limitPerPage * page;
+    const products = await Product.find().limit(productsToShow);
+    if(!token) {
+        
+        return res.render("productPage.ejs", {
+            products,
+            page, 
+            totalProducts,
+            totalPages,
+            limitPerPage,
+            productsToShow
+        } );
     }
 
     const validUser = jwt.verify(token, process.env.SECRET_KEY)
