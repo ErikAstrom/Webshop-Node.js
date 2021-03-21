45065ad0dd0f26c3486700263d229cc3b63782a0
FredrikAndre <fredrik.he.andre@gmail.com>
2021-03-21 14:35:25 +0100

    Added remove function in shoppingcart

--- routes/shoppingRoute.js
+++ routes/shoppingRoute.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const verifyUser = require("../middleware/verifyUser");
-const { addtoCart, showShoppingCart } = require("../controllers/shoppingController");
+const { addtoCart, showShoppingCart, removeFromCart } = require("../controllers/shoppingController");
 
 // Add to shoppingcart
 router.post("/addToCart", verifyUser, addtoCart); 
 
 // Show shopping cart
 router.get("/myShoppingCart", verifyUser, showShoppingCart); 
+
+// Remove item from cart
+router.get("/removeFromCart/:id", verifyUser, removeFromCart);
 
 module.exports = router;
